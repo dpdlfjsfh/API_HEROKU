098011fe6f32dfbd3604a693cac65ef14eacb906
dpdlfjsfh <dpdlfjsfh@naver.com>
2023-05-26 16:35:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -46,6 +46,61 @@ courses = [
     }
 ]
 
+flights = [
+    {
+        "id": 1,
+        "airline": "대한항공",
+        "departure" : "Seoul",
+        "destination": "Jeju",
+        "departure_time": "2023-06-01 09:00",
+        "price": 100000
+    },
+    {
+        "id": 2,
+        "airline": "아시아나항공",
+        "departure" : "Seoul",
+        "destination": "Jeju",
+        "departure_time": "2023-06-01 10:00",
+        "price": 80000
+    },
+    {
+        "id": 3,
+        "airline": "제주항공",
+        "departure" : "Seoul",
+        "destination": "Jeju",
+        "departure_time": "2023-06-01 11:00",
+        "price": 50000
+    }
+]
+
+# 가상의 인터넷 소설 데이터
+novels = [
+    {"title": "The Legendary Guardian", "author": "Shi Luo Ye", "downloads": 100000, "genre": "Fantasy", "release_year": 2015, "platform": "Webnovel"},
+    {"title": "Rebirth of the Thief Who Roamed the World", "author": "Mad Snail", "downloads": 80000, "genre": "Game", "release_year": 2010, "platform": "Webnovel"},
+    {"title": "Lord of the Mysteries", "author": "Cuttlefish That Loves Diving", "downloads": 90000, "genre": "Mystery", "release_year": 2017, "platform": "Qidian"},
+    {"title": "The King's Avatar", "author": "Butterfly Blue", "downloads": 120000, "genre": "Game", "release_year": 2011, "platform": "Webnovel"},
+    {"title": "The Great Ruler", "author": "Tian Can Tu Dou", "downloads": 70000, "genre": "Fantasy", "release_year": 2012, "platform": "Qidian"},
+    {"title": "Release That Witch", "author": "Er Mu", "downloads": 95000, "genre": "Fantasy", "release_year": 2014, "platform": "Webnovel"},
+    {"title": "The Legendary Mechanic", "author": "Chocolion", "downloads": 85000, "genre": "Game", "release_year": 2016, "platform": "Qidian"},
+    {"title": "Martial God Asura", "author": "Kindhearted Bee", "downloads": 110000, "genre": "Fantasy", "release_year": 2013, "platform": "Webnovel"},
+    {"title": "I Shall Seal the Heavens", "author": "Er Gen", "downloads": 75000, "genre": "Fantasy", "release_year": 2014, "platform": "Qidian"},
+    {"title": "The Great Thief", "author": "Boating Lyrics", "downloads": 85000, "genre": "Game", "release_year": 2015, "platform": "Webnovel"},
+]
+
+# 가상의 영화 데이터
+movies = [
+    Movie(id=1, title="Inception", director="Christopher Nolan",spectator=100000, genre="Sci-Fi", release_year=2010),
+    Movie(id=2, title="The Shawshank Redemption", director="Frank Darabont",spectator=120000, genre="Drama", release_year=1994),
+    Movie(id=3, title="Pulp Fiction", director="Quentin Tarantino",spectator=130000, genre="Crime", release_year=1994),
+    Movie(id=4, title="The Godfather", director="Francis Ford Coppola",spectator=170000, genre="Crime", release_year=1972),
+    Movie(id=5, title="The Dark Knight", director="Christopher Nolan",spectator=160000, genre="Action", release_year=2008),
+    Movie(id=6, title="Fight Club", director="David Fincher",spectator=140000, genre="Drama", release_year=1999),
+    Movie(id=7, title="Interstellar", director="Christopher Nolan",spectator=130000, genre="Sci-Fi", release_year=2014),
+    Movie(id=8, title="Goodfellas", director="Martin Scorsese",spectator=140000, genre="Crime", release_year=1990),
+    Movie(id=9, title="The Matrix", director="Lana Wachowski,spectator=120000, Lilly Wachowski", genre="Sci-Fi", release_year=1999),
+    Movie(id=10, title="Seven", director="David Fincher",spectator=150000, genre="Crime", release_year=1995),
+]
+
 @app.get("/courses")
 async def get_courses(
     id: int = Query(default=None),
@@ -88,4 +143,106 @@ async def get_courses(
 
     return filtered_courses
 
+@app.get("/flights")
+async def get_flights(
+    id: int = Query(default=None),
+    airline: str = Query(default=None),
+    departure: str = Query(default=None),
+    destination: str = Query(default=None),
+    min_price: int = Query(default=None, gt=0),
+    max_price: int = Query(default=None, gt=0),
+):
+    filtered_flights = flights
 
+    if id is not None:
+        # id 필터링
+        filtered_flights = [flight for flight in filtered_flights if flight["id"] == id]
+
+    if airline is not None:
+        # 항공사명 포함 필터링
+        filtered_flights = [flight for flight in filtered_flights if airline.lower() in flight["airline"].lower()]
+
+    if departure is not None:
+        # 출발지 포함 필터링
+        filtered_flights = [flight for flight in filtered_flights if departure.lower() in flight["departure"].lower()]
+        
+    if destination is not None:
+        # 도착지 포함 필터링
+        filtered_flights = [flight for flight in filtered_flights if destination.lower() in flight["destination"].lower()]
+
+    if min_price is not None:
+        # 최소 가격 필터링
+        filtered_flights = [flight for flight in filtered_flights if flight["price"] >= min_price]
+        
+    if max_price is not None:
+        # 최대 가격 필터링
+        filtered_flights = [flight for flight in filtered_flights if flight["price"] <= max_price]
+
+    return filtered_flights
+
+@app.get("/movies")
+async def get_movies(
+    title: str = Query(default=None),
+    director: str = Query(default=None),
+    genre: str = Query(default=None),
+    release_year: int = Query(default=None, ge=0)
+):
+    filtered_movies = movies
+
+    if title is not None:
+        # 제목 포함 필터링
+        filtered_movies = [movie for movie in filtered_movies if title.lower() in movie.title.lower()]
+
+    if director is not None:
+        # 감독 포함 필터링
+        filtered_movies = [movie for movie in filtered_movies if director.lower() in movie.director.lower()]
+
+    if genre is not None:
+        # 장르 필터링
+        filtered_movies = [movie for movie in filtered_movies if genre.lower() == movie.genre.lower()]
+
+    if release_year is not None:
+        # 개봉 연도 필터링
+        filtered_movies = [movie for movie in filtered_movies if movie.release_year == release_year]
+    
+    if min_spectator is not None:
+        # 최소 관객 필터링
+        filtered_flights = [movie for movie in filtered_movies if movie["spectator"] >= min_spectator]
+        
+    if max_spectator is not None:
+        # 최대 관중 필터링
+        filtered_flights = [movie for movie in filtered_movies if movie["spectator"] <= max_spectator]
+
+    return filtered_movies
+
+@app.get("/novels")
+async def get_novels(
+    title: str = Query(default=None),
+    author: str = Query(default=None),
+    platform: str = Query(default=None),
+    genre: str = Query(default=None),
+    min_downloads: int = Query(default=None, ge=0)
+):
+    filtered_novels = novels
+
+    if title is not None:
+        # 제목 필터링
+        filtered_novels = [novel for novel in filtered_novels if title.lower() in novel['title'].lower()]
+
+    if author is not None:
+        # 작가 필터링
+        filtered_novels = [novel for novel in filtered_novels if author.lower() in novel['author'].lower()]
+
+    if platform is not None:
+        # 플랫폼 필터링
+        filtered_novels = [novel for novel in filtered_novels if platform.lower() == novel['platform'].lower()]
+
+    if genre is not None:
+        # 장르 필터링
+        filtered_novels = [novel for novel in filtered_novels if genre.lower() == novel['genre'].lower()]
+
+    if min_downloads is not None:
+        # 최소 다운로드 필터링
+        filtered_novels = [novel for novel in filtered_novels if novel['downloads'] >= min_downloads]
+
+    return filtered_novels
